3007e3de3a6a0645dc1b0ecd31ea5e5964bb4d95
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-24 01:42:05 +0700

    debug

--- train_multi.py
+++ train_multi.py
@@ -49,7 +49,8 @@ def train(rank, world_size, opt):
                 
                 predict_mask = model.forward_only(visible_mask)
                 predict_mask = tensor2im(predict_mask).squeeze()
-                final_mask = tensor2im(predict_mask).squeeze()
+                final_mask = tensor2im(final_mask).squeeze()
+                print(predict_mask)
                 intersection = ((predict_mask == 1) & (final_mask == 1)).sum()
                 predict_area = (predict_mask == 1).sum()
                 target_area = (final_mask == 1).sum()
