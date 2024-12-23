78f416c2a221b8ffaf70b08fe06ae993438c50e3
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 23:42:28 +0700

    debug

--- src/models/networks.py
+++ src/models/networks.py
@@ -111,6 +111,7 @@ def init_net(net, init_type='normal', init_gain=0.02, rank=None, is_dpp=False):
     """
     if is_dpp:
         assert(torch.cuda.is_available())
+        net.to(torch.device(f"cuda:{rank}"))
         net = DDP(net, device_ids=[rank])
     init_weights(net, init_type, init_gain=init_gain)
     return net
