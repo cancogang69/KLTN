f54ea081feceaf3ced84ea60a494ee3e01fb15d7
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 23:46:45 +0700

    debug

--- src/models/pix2pix_model.py
+++ src/models/pix2pix_model.py
@@ -85,7 +85,7 @@ class Pix2PixModel(BaseModel):
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         torch_resize = Resize(size=self.target_shape)
-        self.fake_B = torch_resize(self.netG(self.real_A))  # G(A)
+        self.fake_B = torch_resize(self.netG(self.real_A)).to(self.device) # G(A)
 
 
     def forward_only(self, input):
