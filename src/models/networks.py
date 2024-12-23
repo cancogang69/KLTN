fd028a75da605685580bea5485ac7a38484c02e1
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 22:11:54 +0700

    debug

--- src/models/networks.py
+++ src/models/networks.py
@@ -337,7 +337,7 @@ class ResnetGenerator(nn.Module):
         else:
             use_bias = norm_layer == nn.InstanceNorm2d
 
-        model = [#nn.ReflectionPad2d(3),
+        model = [nn.ReflectionPad2d(3),
                  nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
                  norm_layer(ngf),
                  nn.ReLU(True)]
@@ -362,7 +362,7 @@ class ResnetGenerator(nn.Module):
                                          bias=use_bias),
                       norm_layer(int(ngf * mult / 2)),
                       nn.ReLU(True)]
-        # model += [nn.ReflectionPad2d(3)]
+        model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
         model += [nn.Tanh()]
 
