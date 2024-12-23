c3d6f4f637772d254be69e5abee4429dbbeddf96
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 20:12:16 +0700

    debug

--- src/options/custom_options.py
+++ src/options/custom_options.py
@@ -5,6 +5,7 @@ def parse_args():
     # network saving and loading parameters
     parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
     parser.add_argument('--model', type=str, default='cycle_gan', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+    parser.add_argument('--gpu-ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
     parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
     parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
     parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
