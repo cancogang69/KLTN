fe765930f6ee0df23e601e7a10b6923baea66f14
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 20:23:21 +0700

    debug

--- src/models/networks.py
+++ src/models/networks.py
@@ -111,7 +111,7 @@ def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
     """
     if len(gpu_ids) > 0:
         assert(torch.cuda.is_available())
-        devices = [f"cuda:{idx}" for idx in gpu_ids]
+        devices = [f"cuda:{idx}" for idx in gpu_ids.split(",")]
         net.to(devices[0])
         net = torch.nn.DataParallel(net, devices)  # multi-GPUs
     init_weights(net, init_type, init_gain=init_gain)
