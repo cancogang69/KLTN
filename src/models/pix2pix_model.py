dbf4f25cc2b00a526e7c644e019ab9f2db5b1d68
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-23 23:50:31 +0700

    debug

--- src/models/pix2pix_model.py
+++ src/models/pix2pix_model.py
@@ -103,7 +103,7 @@ class Pix2PixModel(BaseModel):
         # Fake; stop backprop to the generator by detaching fake_B
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
         pred_fake = self.netD(fake_AB.detach())
-        self.loss_D_fake = self.criterionGAN(pred_fake, False)
+        self.loss_D_fake = self.criterionGAN(pred_fake.to(self.device), False)
         # Real
         real_AB = torch.cat((self.real_A, self.real_B), 1)
         pred_real = self.netD(real_AB)
