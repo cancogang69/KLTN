427c698f3d8e6e7d41e9609009982e4009e3baed
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-28 15:02:46 +0700

    debug

--- train.py
+++ train.py
@@ -1,84 +1,142 @@
-"""General-purpose training script for image-to-image translation.
-
-This script works for various models (with option '--model': e.g., pix2pix, cyclegan, colorization) and
-different datasets (with option '--dataset_mode': e.g., aligned, unaligned, single, colorization).
-You need to specify the dataset ('--dataroot'), experiment name ('--name'), and model ('--model').
-
-It first creates model, dataset, and visualizer given the option.
-It then does standard network training. During the training, it also visualize/save the images, print/save the loss plot, and save models.
-The script supports continue/resume training. Use '--continue_train' to resume your previous training.
-
-Example:
-    Train a CycleGAN model:
-        python train.py --dataroot ./datasets/maps --name maps_cyclegan --model cycle_gan
-    Train a pix2pix model:
-        python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --direction BtoA
-
-See options/base_options.py and options/train_options.py for more training options.
-See training and test tips at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/tips.md
-See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/docs/qa.md
-"""
+import os
 import time
+from PIL import Image
+
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+from src.datasets.custom_dataset import CustomDataset
 from src.options.custom_options import parse_args
-from src.datasets.data_loader import DatasetLoader
 from src.models import create_model
+from src.utils.util import tensor2im
 
 
-if __name__ == '__main__':
-    opt = parse_args()
+def train(rank, opt):
+    checkpoint_save_path = f"checkpoints/{opt.name}"
+
     opt.isTrain = True
-    opt.rank = "0"
-    train_dataset = DatasetLoader(anno_path=opt.train_anno_path)
-    val_dataset = DatasetLoader(anno_path=opt.val_anno_path)
+    opt.rank = rank
+    opt.is_ddp = False
 
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
-    total_iters = 0                # the total number of training iterations
+    train_dataset = CustomDataset(opt.train_anno_path, opt)
+    val_dataset = CustomDataset(opt.val_anno_path, opt)
+
+    train_loader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=opt.is_shuffle)
+    val_loader = DataLoader(val_dataset, batch_size=opt.batch_size, shuffle=False)
+
+    model = create_model(opt)
+    model.setup(opt)
+
+    if not os.path.exists(opt.plot_save_path) and rank == 0:
+        os.makedirs(opt.plot_save_path)
+
+    if not os.path.exists(checkpoint_save_path) and rank == 0:
+        os.makedirs(checkpoint_save_path)
 
     best_miou = 0
+    result_count = 5
+    epoch_losses = {"gen": [], "dis": []}
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
         epoch_start_time = time.time()          
-        model.update_learning_rate()    
-        for data in train_dataset:
-            visible_mask, invisible_mask, final_mask, _ = data
-            model.set_input(input=visible_mask, target=final_mask)
-            model.optimize_parameters()
+            
+        this_epoch_losses = {"num": 0, "gen": 0, "dis": 0}
 
-            iter_data_time = time.time()
+        is_discrim_backprop = ((epoch % opt.discrim_backprop_freq) == 0)
+        for data in train_loader:
+            input_datas, final_masks, _ = data
+            model.set_input(input=input_datas, target=final_masks)
+            gen_loss, dis_loss = model.optimize_parameters(is_discrim_backprop)
+
+            this_epoch_losses["num"] += 1
+            this_epoch_losses["gen"] += gen_loss
+            this_epoch_losses["dis"] += dis_loss
+
+        epoch_losses["gen"].append(this_epoch_losses["gen"]/this_epoch_losses["num"])
+        epoch_losses["dis"].append(this_epoch_losses["dis"]/this_epoch_losses["num"])
+
+        if rank == 0:
+            fig, axs = plt.subplots(1, 1)
+            axs.set_xlabel("epoch")
+            axs.set_ylabel("loss")
+            axs.set_title("Loss per epoch")
+
+            epoch_nums = range(1, len(epoch_losses["gen"])+1)
+            axs.plot(epoch_nums, epoch_losses["gen"], label="generator_loss")
+            axs.plot(epoch_nums, epoch_losses["dis"], label="discriminator_loss")
+            axs.xaxis.get_major_locator().set_params(integer=True)
+            axs.legend()
+            fig.savefig(f"{opt.plot_save_path}/losses_plot.jpg")
+            plt.close(fig)
+
+        model.update_learning_rate()
 
         if epoch % opt.val_freq == 0:
             total_iou = 0
             percents_iou = {}
-            for data in val_dataset:
-                visible_mask, invisible_mask, final_mask, percent = data
+            results = []
+            for data in val_loader:
                 
-                predict_mask = model.forward_only(visible_mask)
+                input_datas, final_masks, percents = data
+                predict_masks = model.forward_only(input_datas)
 
-                intersection = ((predict_mask == 1) & (final_mask == 1)).sum()
-                predict_area = (predict_mask == 1).sum()
-                target_area = (final_mask == 1).sum()
-                iou = intersection / (predict_area + target_area - intersection)
-                if percent not in percents_iou:
-                    percents_iou[percent] = [0, 0]
+                for predict_mask, final_mask, percent in zip(predict_masks, final_masks, percents):
+                    predict_mask = tensor2im(predict_mask).squeeze()
+                    final_mask = tensor2im(final_mask).squeeze()
+                    intersection = ((predict_mask == 1) & (final_mask == 1)).sum()
+                    predict_area = (predict_mask == 1).sum()
+                    target_area = (final_mask == 1).sum()
+                    iou = intersection / (predict_area + target_area - intersection)
 
-                percents_iou[percent][0] += 1
-                percents_iou[percent][1] += iou
-                total_iou += iou
+                    percent = percent.item()
+                    if percent not in percents_iou:
+                        percents_iou[percent] = [0, 0]
 
-            m_iou = total_iou / val_dataset.anno_len
+                    percents_iou[percent][0] += 1
+                    percents_iou[percent][1] += iou
+                    total_iou += iou
+
+                    if len(results) < result_count:
+                        results.append([predict_mask, final_mask])
+
+            m_iou = total_iou / len(val_dataset)
             for percent, values in percents_iou.items():
                 percents_iou[percent] = values[1] / values[0]
 
-            if best_miou < m_iou:
-                best_miou = m_iou
+            if rank == 0:
+                save_best = False
+                if best_miou < m_iou:
+                    best_miou = m_iou
+                    save_best = True
+                nrows = 2
+                ncols = result_count
+                fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8, 2))
+                plt.suptitle(f"EPOCH : {epoch}")
+                
+                for i, result in enumerate(results):
+                    axes[0][i].imshow(Image.fromarray(result[0]), cmap="gray")
+                    axes[0][i].axis("off")
+                    axes[1][i].imshow(Image.fromarray(result[1]), cmap="gray")
+                    axes[1][i].axis("off")
 
-            print(f"Best mean IoU: {best_miou}, this epoch mean IoU: {m_iou}")
-            for percent, m_iou in percents_iou.items():
-                print(f"percent {percent}, mean IoU: {m_iou}")
+                fig.savefig(f"{opt.plot_save_path}/epoch_{epoch}_result.jpg")
+                plt.close(fig)
 
-            if epoch % opt.save_epoch_freq == 0:
-                print(f"saving the model at the end of epoch {epoch}")
-                model.save_networks('latest')
-                model.save_networks(epoch)
+                print(f"Best mean IoU: {best_miou}, this epoch mean IoU: {m_iou}")
+                for percent, m_iou in percents_iou.items():
+                    print(f"percent {percent}, mean IoU: {m_iou}")
 
-        print(f"End of epoch {epoch} / {opt.n_epochs + opt.n_epochs_decay} \t Time Taken: {time.time() - epoch_start_time} sec")
+                if save_best:
+                    print(f"saving the model at the end of epoch {epoch}")
+                    model.save_networks("best")
+
+                model.save_networks("last")
+
+            
+
+        if rank == 0:
+            print(f"End of epoch {epoch} / {opt.n_epochs + opt.n_epochs_decay} \t Time Taken: {time.time() - epoch_start_time} sec")
+
+
+if __name__ == "__main__":
+    opt = parse_args()
+    train(0, opt)
