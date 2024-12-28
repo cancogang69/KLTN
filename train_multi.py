676d50a1f32add5ac2b57218984863769e6dc422
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2024-12-28 21:39:33 +0700

    debug

--- train_multi.py
+++ train_multi.py
@@ -14,6 +14,41 @@ from src.models import create_model
 from src.utils.util import tensor2im
 
 
+def validate(model, val_dataset, val_loader, result_count=5):
+    total_iou = 0
+    percents_iou = {}
+    results = []
+    for data in val_loader:
+        
+        input_datas, final_masks, percents = data
+        predict_masks = model.forward_only(input_datas)
+
+        for predict_mask, final_mask, percent in zip(predict_masks, final_masks, percents):
+            predict_mask = tensor2im(predict_mask).squeeze()
+            final_mask = tensor2im(final_mask).squeeze()
+            intersection = ((predict_mask == 1) & (final_mask == 1)).sum()
+            predict_area = (predict_mask == 1).sum()
+            target_area = (final_mask == 1).sum()
+            iou = intersection / (predict_area + target_area - intersection)
+
+            percent = percent.item()
+            if percent not in percents_iou:
+                percents_iou[percent] = [0, 0]
+
+            percents_iou[percent][0] += 1
+            percents_iou[percent][1] += iou
+            total_iou += iou
+
+            if len(results) < result_count:
+                results.append([predict_mask, final_mask])
+
+    m_iou = total_iou / len(val_dataset)
+    for percent, values in percents_iou.items():
+        percents_iou[percent] = values[1] / values[0]
+
+    return m_iou, results, percents_iou
+
+
 def train(rank, world_size, opt):
     dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
     checkpoint_save_path = f"{opt.save_path}/{opt.name}"
@@ -33,14 +68,25 @@ def train(rank, world_size, opt):
     model = create_model(opt)
     model.setup(opt)
 
+    best_miou = 0
+    result_count = 5
+    if opt.model_generator_path is not None:
+        m_iou, _, percents_iou = validate(model, val_dataset, val_loader, result_count)
+        if rank == 0:
+            save_best = False
+            if best_miou < m_iou:
+                best_miou = m_iou
+
+            print(f"Best mean IoU: {best_miou}, this epoch mean IoU: {m_iou}")
+            for percent, m_iou in percents_iou.items():
+                print(f"percent {percent}, mean IoU: {m_iou}")
+
     if not os.path.exists(opt.plot_save_path) and rank == 0:
         os.makedirs(opt.plot_save_path)
 
     if not os.path.exists(checkpoint_save_path) and rank == 0:
         os.makedirs(checkpoint_save_path)
 
-    best_miou = 0
-    result_count = 5
     epoch_losses = {"gen": [], "dis": []}
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
         epoch_start_time = time.time()          
@@ -77,36 +123,7 @@ def train(rank, world_size, opt):
         model.update_learning_rate()
 
         if epoch % opt.val_freq == 0:
-            total_iou = 0
-            percents_iou = {}
-            results = []
-            for data in val_loader:
-                
-                input_datas, final_masks, percents = data
-                predict_masks = model.forward_only(input_datas)
-
-                for predict_mask, final_mask, percent in zip(predict_masks, final_masks, percents):
-                    predict_mask = tensor2im(predict_mask).squeeze()
-                    final_mask = tensor2im(final_mask).squeeze()
-                    intersection = ((predict_mask == 1) & (final_mask == 1)).sum()
-                    predict_area = (predict_mask == 1).sum()
-                    target_area = (final_mask == 1).sum()
-                    iou = intersection / (predict_area + target_area - intersection)
-
-                    percent = percent.item()
-                    if percent not in percents_iou:
-                        percents_iou[percent] = [0, 0]
-
-                    percents_iou[percent][0] += 1
-                    percents_iou[percent][1] += iou
-                    total_iou += iou
-
-                    if len(results) < result_count:
-                        results.append([predict_mask, final_mask])
-
-            m_iou = total_iou / len(val_dataset)
-            for percent, values in percents_iou.items():
-                percents_iou[percent] = values[1] / values[0]
+            m_iou, results, percents_iou = validate(model, val_dataset, val_loader, result_count)
 
             if rank == 0:
                 save_best = False
