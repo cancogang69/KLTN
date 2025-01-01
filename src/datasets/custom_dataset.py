baf07d9c8e139617cc1a031dcbe89b7c557a73ef
cancogang69 <90518328+Cancogang69@users.noreply.github.com>
2025-01-01 11:41:21 +0700

    debug

--- src/datasets/custom_dataset.py
+++ src/datasets/custom_dataset.py
@@ -91,7 +91,8 @@ class CustomDataset(object):
             # expand_map = self.transform_grayscale_img(Image.fromarray(expand_map))
             expand_region = expand_map.copy()
             expand_region[expand_region == -1] = 0
-            expand_map = self.input_resize(torch.Tensor(expand_map))
+            expand_region = self.input_resize(torch.as_tensor(expand_region))
+            expand_map = self.input_resize(torch.as_tensor(expand_map))
         if "label" in self.opt.extra_info:
             label_segment = self.__get_label_segment(visible_mask, anno["category_id"])
             label_segment = self.transform_label_mask(torch.Tensor(label_segment))
